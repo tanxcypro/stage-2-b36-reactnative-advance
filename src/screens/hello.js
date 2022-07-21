ffdde044e2555f5d1e38b15898550b79a6fb1167
Your Name <samsulr99@gmail.com>
2022-07-21 09:51:00 +0700

    update

--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { Text, Box } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({navigation}) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
@@ -10,6 +11,19 @@ export default function Hello() {
       </Text>
 
       {/* Create Button */}
+      <TouchableOpacity onPress={()=> navigation.navigate("IncDec")}
+        style={{
+          backgroundColor: 'indigo',
+          height: 40,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 10,
+          margin: 10
+        }}
+      >
+        <Text textColor={{color: 'white'}}>Screen Increment Decrement</Text>
+      </TouchableOpacity>
     </Box>
   );
 }
